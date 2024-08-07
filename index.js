4b7332aeb6031a7e3f5a11b181c4f2b5629f9b90
Simon Tadesse <simonzagwe@gmail.com>
2024-08-07 09:20:54 -0800

    just added sum

--- index.js
+++ index.js
@@ -187,6 +187,19 @@ document.getElementById("subtax").onclick = function(){
 
 }
 ////////////////////////////////////////////////////////
+/////////sum///////////
+// script.js
+function calculateSum() {
+    const number1 = parseFloat(document.getElementById('number1').value);
+    const number2 = parseFloat(document.getElementById('number2').value);
+    
+    if (isNaN(number1) || isNaN(number2)) {
+        document.getElementById('result').innerText = 'Please enter valid numbers';
+    } else {
+        const sum = number1 + number2;
+        document.getElementById('result').innerText = 'Result: ' + sum;
+    }
+}
 
  
 
