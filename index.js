7ea5d0e6d77ec86d173efb1294fce9288a3d72c4
Simon Tadesse <simonzagwe@gmail.com>
2024-08-12 11:59:09 -0800

    grade

--- index.js
+++ index.js
@@ -249,6 +249,28 @@ function convertKmToMiles() {
 
 
 
+function calculateGrade() {
+    const marks = document.getElementById('marks').value;
+    let grade;
+
+    if (marks >= 90) {
+        grade = 'A+';
+    } else if (marks >= 80) {
+        grade = 'A';
+    } else if (marks >= 70) {
+        grade = 'B';
+    } else if (marks >= 60) {
+        grade = 'C';
+    } else if (marks >= 50) {
+        grade = 'D';
+    } else if (marks >= 40) {
+        grade = 'E';
+    } else {
+        grade = 'F';
+    }
+
+    document.getElementById('result').innerText = `Your grade is: ${grade}`;
+}
 
 
 
